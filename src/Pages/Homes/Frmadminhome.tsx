15503b04a9645ab27b66f062037c750d4d8bac89
Thiraphat thamprayot <Thiraphat.th@ku.th>
2025-03-12 13:46:40 +0700

    เพิ่มรีโหลด

--- src/Pages/Homes/Frmadminhome.tsx
+++ src/Pages/Homes/Frmadminhome.tsx
@@ -74,6 +74,37 @@ function Frmadminhome() {
     Showlistjobdiscription();
   }, [inputdata.Categoryid]);
 
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+
+    // ฟังก์ชันที่ใช้รีโหลดหน้าจอ
+    const reloadPage = () => {
+      window.location.reload();
+    };
+
+    // ฟังก์ชันที่รีเซ็ตตัวจับเวลาเมื่อมีการทำกิจกรรม
+    const resetTimeout = () => {
+      clearTimeout(timeout); // ลบ timeout เดิม
+      timeout = setTimeout(reloadPage, 20000); // รีโหลดหน้าจอหลังจาก 20 วินาทีไม่มีการทำกิจกรรม
+    };
+
+    // ตั้ง listener เพื่อตรวจจับการเคลื่อนไหวของเมาส์และการกดแป้นพิมพ์
+    document.addEventListener("mousemove", resetTimeout);
+    document.addEventListener("keydown", resetTimeout);
+
+    // เริ่มต้นการนับเวลา
+    resetTimeout();
+
+    // ลบ event listeners เมื่อ component ถูก unmount
+    return () => {
+      clearTimeout(timeout);
+      document.removeEventListener("mousemove", resetTimeout);
+      document.removeEventListener("keydown", resetTimeout);
+    };
+  }, []);
+
+  
   async function showlistuser() {
     axios.get(API.returnURL.url + "Personnel").then(function (response) {
       setlistuser(response.data);
